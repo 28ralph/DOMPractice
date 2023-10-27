b22b208529318718973ec792b02ed8f984fb4f46
28ralph <lee.ralph@sbcglobal.net>
2023-10-27 14:45:19 -0500

    DOM Practice

---
+++ dompractice.js
@@ -0,0 +1,121 @@
+const parent = document.querySelector(".Parent");
+parent.style.width = "500px";
+parent.style.height = "450px";
+parent.style.backgroundColor = "red";
+
+const child1 = document.querySelector(".child1");
+child1.style.width = "100%";
+child1.style.height = "50px";
+child1.style.backgroundColor = 'blue';
+child1.style.marginTop = '30px';
+child1.style.marginBottom = "30px";
+child1.style.marginLeft = "0px";
+child1.style.marginRight = "0px";
+child1.style.textAlign = "center";
+child1.style.fontColor = "#E0D635"
+// child1.style.textAlign="center";
+
+const child2 = document.querySelector(".child2");
+child2.style.width = "100%";
+child2.style.height = "50px";
+child2.style.backgroundColor = 'blue';
+child2.style.marginTop = '30px';
+child2.style.marginBottom = "30px";
+child2.style.marginLeft = "0px";
+child2.style.marginRight = "0px";
+child2.style.textAlign = "center";
+child2.style.fontColor = "#E0D635"
+
+const child3 = document.querySelector(".child3");
+child3.style.width = "100%";
+child3.style.height = "50px";
+child3.style.backgroundColor = 'blue';
+child3.style.marginTop = '30px';
+child3.style.marginBottom = "30px";
+child3.style.marginLeft = "0px";
+child3.style.marginRight = "0px";
+child3.style.textAlign = "center";
+child3.style.fontColor = "#E0D635"
+
+const child4 = document.querySelector(".child4");
+child4.style.width = "100%";
+child4.style.height = "50px";
+child4.style.backgroundColor = 'blue';
+child4.style.marginTop = '30px';
+child4.style.marginBottom = "30px";
+child4.style.marginLeft = "0px";
+child4.style.marginRight = "0px";
+child4.style.textAlign = "center";
+child4.style.fontColor = "#E0D635"
+
+const child5 = document.querySelector(".child5");
+child5.style.width = "100%";
+child5.style.height = "50px";
+child5.style.backgroundColor = 'blue';
+child5.style.marginTop = '30px';
+child5.style.marginBottom = "30px";
+child5.style.marginLeft = "0px";
+child5.style.marginRight = "0px";
+child5.style.textAlign = "center";
+child5.style.fontColor = "#E0D635"
+
+const child6 = document.querySelector(".child6");
+child6.style.width = "100%";
+child6.style.height = "50px";
+child6.style.backgroundColor = 'blue';
+child6.style.marginTop = '30px';
+child6.style.marginBottom = "30px";
+child6.style.marginLeft = "0px";
+child6.style.marginRight = "0px";
+child6.style.textAlign = "center";
+child6.style.fontColor = "#E0D635"
+
+const child1grand1 = document.querySelector(".child1-grand1")
+child1grand1.style.width = "100px";
+child1grand1.style.height = "7px";
+child1grand1.style.marginTop = "10px";
+
+const child1grand2 = document.querySelector(".child1-grand2")
+child1grand2.style.width = "100px";
+child1grand2.style.height = "7px";
+child1grand2.style.marginTop = "10px";
+
+const child1grand3 = document.querySelector(".child1-grand3")
+child1grand3.style.width = "100px";
+child1grand3.style.height = "7px";
+child1grand3.style.marginTop = "10px";
+
+const child1grand4 = document.querySelector(".child1-grand4")
+child1grand4.style.width = "100px";
+child1grand4.style.height = "7px";
+child1grand4.style.marginTop = "10px";
+
+const child1grand5 = document.querySelector(".child1-grand5")
+child1grand5.style.width = "100px";
+child1grand5.style.height = "7px";
+child1grand5.style.marginTop = "10px";
+
+const child1grand6 = document.querySelector(".child1-grand6")
+child1grand6.style.width = "100px";
+child1grand6.style.height = "7px";
+child1grand6.style.marginTop = "10px";
+
+const fourteenth = document.querySelector(".fourteenth")
+fourteenth.style.color = "pink";
+
+const childThree = document.querySelector(".child3");
+childThree.style.position = "relative";
+
+const blackSheep = document.querySelectorAll("child3-grand1")[2];
+blackSheep.style.color = "black";
+blackSheep.style.fontSize = "9rem";
+blackSheep.style.textAlign = "right";
+// blackSheep.style.transform = "scale(1, -1)"
+blackSheep.style.position = "absolute";
+blackSheep.style.top = "30px";
+blackSheep.style.left = "500px";
+
+
+
+
+
